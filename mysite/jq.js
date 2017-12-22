8f67272b5b813d865324dab9ca8898676f61218d
YukoSugar <34759540+YukoSugar@users.noreply.github.com>
2017-12-22 12:21:18 +0900

    mysite
    mysite

---
+++ mysite/jq.js
@@ -0,0 +1,107 @@
+// old //
+
+$(function(){
+  
+  $('.o1').mouseover(function () {
+    $(".old-1").css("background-image","url(img/hagoita.jpg)");
+  }).mouseout(function(){
+
+   $(".old-1").css("background","");
+
+  });
+
+    $('.o2').mouseover(function () {
+    $(".old-1").css("background-image","url(img/karuta.jpg)");
+  }).mouseout(function(){
+
+   $(".old-1").css("background","");
+
+  });
+
+    $('.o3').mouseover(function () {
+    $(".old-1").css("background-image","url(img/koma.jpg)");
+  }).mouseout(function(){
+
+   $(".old-1").css("background","");
+
+  });
+
+    $('.o4').mouseover(function () {
+    $(".old-1").css("background-image","url(img/saikoro.jpg)");
+  }).mouseout(function(){
+
+   $(".old-1").css("background","");
+
+  });
+
+    $('.o5').mouseover(function () {
+    $(".old-1").css("background-image","url(img/tako.jpg)");
+  }).mouseout(function(){
+
+   $(".old-1").css("background","");
+
+  });
+
+    $('.o6').mouseover(function () {
+    $(".old-1").css("background-image","url(img/daruma-o.jpg)");
+  }).mouseout(function(){
+
+   $(".old-1").css("background","");
+
+  });
+
+});
+
+// new //
+
+$(function(){
+  
+  $('.n1').mouseover(function () {
+    $(".now-1").css("background-image","url(img/cards.jpg)");
+  }).mouseout(function(){
+
+   $(".now-1").css("background","");
+
+  });
+
+    $('.n2').mouseover(function () {
+    $(".now-1").css("background-image","url(img/game.jpg)");
+  }).mouseout(function(){
+
+   $(".now-1").css("background","");
+
+  });
+
+    $('.n3').mouseover(function () {
+    $(".now-1").css("background-image","url(img/puzzle.jpg)");
+  }).mouseout(function(){
+
+   $(".now-1").css("background","");
+
+  });
+
+    $('.n4').mouseover(function () {
+    $(".now-1").css("background-image","url(img/phone.jpg)");
+  }).mouseout(function(){
+
+   $(".now-1").css("background","");
+
+  });
+
+    $('.n5').mouseover(function () {
+    $(".now-1").css("background-image","url(img/car.jpg)");
+  }).mouseout(function(){
+
+   $(".now-1").css("background","");
+
+  });
+
+    $('.n6').mouseover(function () {
+    $(".now-1").css("background-image","url(img/bouldering.jpg)");
+  }).mouseout(function(){
+
+   $(".now-1").css("background","");
+
+  });
+
+});
